db3d7b85ea9e4fd1942e9394f9a4894bf5fed808
xuedongli <18236925857@163.com>
2024-03-29 17:17:03 +0800

    Update NavAPIButtons-4.vue

--- vue-demo/src/views/NavAPIButtons-4.vue
+++ vue-demo/src/views/NavAPIButtons-4.vue
@@ -3,5 +3,24 @@
 
   通过过滤能不能自动选择串口，然后点击按钮可以直接选中对应串口？
  -->
+<template>
+  <div class="about">
+    <input type="text" v-model="servalData" name="" id="" hidden>
+    <p>条码数据：{{ servalData }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const servalData = ref('')
+
+
+</script>
+
+<style>
+
+</style>
+
 
 
