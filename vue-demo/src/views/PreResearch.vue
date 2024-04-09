6170b30a5240075a7343bcbeb5da76d8a8caac04
CorderLeoD <18236925857@163.com>
2024-04-09 16:02:01 +0800

    edit take photo

--- vue-demo/src/views/PreResearch.vue
+++ vue-demo/src/views/PreResearch.vue
@@ -8,11 +8,11 @@
   <hr />
   <video ref="video" autoplay></video>
   <section>
-    <canvas id="canvas"></canvas>
+    <canvas ref="canvas"></canvas>
   </section>
-  <section><img src="" alt="" id="img" /></section>
+  <section><img src="" alt="" ref="photoImg" /></section>
   <button @click="getCamera">调取摄像头</button>
-  <button @click="tackcapture">拍照</button>
+  <button @click="tackCapture">拍照</button>
 </template>
 
 <script setup>
@@ -60,32 +60,31 @@ function getCamera() {
     alert('您的浏览器不支持摄像头调用')
   }
 }
-function tackcapture() {
+const canvas = ref(null)
+const photoImg = ref(null)
+function tackCapture() {
   // 需要判断媒体流是否就绪
-  const canvas = document.querySelector('#canvas')
-  const video = document.querySelector('#video')
-  const img = document.querySelector('#img')
-  const context = canvas.getContext('2d')
+  const context = canvas.value.getContext('2d')
   const width = 320 // 视频和canvas的宽度
   let height = 0 //
   let streaming = true // 是否开始捕获媒体
   if (streaming) {
-    context.drawImage(video, 0, 0, 350, 200) // 将视频画面捕捉后绘制到canvas里面
-    img.src = canvas.toDataURL('image/png') // 将canvas的数据传送到img里
-    alert(img.src) // 这边的值可以传入后端
+    context.drawImage(video.value, 0, 0, 350, 200) // 将视频画面捕捉后绘制到canvas里面
+    photoImg.value.src = canvas.value.toDataURL('image/png') // 将canvas的数据传送到img里
+    alert(photoImg.value.src) // 这边的值可以传入后端
   }
 
   // 监听视频流就位事件,即视频可以播放了
-  video.addEventListener(
+  video.value.addEventListener(
     'canplay',
     function() {
       if (!streaming) {
-        height = video.videoHeight / (video.videoWidth / width)
+        height = video.value.videoHeight / (video.value.videoWidth / width)
 
-        video.setAttribute('width', width)
-        video.setAttribute('height', height)
-        canvas.setAttribute('width', width)
-        canvas.setAttribute('height', height)
+        video.value.setAttribute('width', width)
+        video.value.setAttribute('height', height)
+        canvas.value.setAttribute('width', width)
+        canvas.value.setAttribute('height', height)
         streaming = true
       }
     },
